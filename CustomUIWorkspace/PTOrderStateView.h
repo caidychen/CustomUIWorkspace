d1fb0313dcfd4253105b32605dc2d6806c060737
CHEN KAIDI <caidychen@hotmail.com>
2015-11-27 16:41:23 +0800

    PTOrderStateView

---
+++ CustomUIWorkspace/PTOrderStateView.h
@@ -0,0 +1,13 @@
+//
+//  PTOrderStateView.h
+//  CustomUIWorkspace
+//
+//  Created by CHEN KAIDI on 27/11/2015.
+//  Copyright © 2015 CHEN KAIDI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PTOrderStateView : UIView
+
+@end
---
+++ CustomUIWorkspace/PTOrderStateView.m
@@ -0,0 +1,21 @@
+//
+//  PTOrderStateView.m
+//  CustomUIWorkspace
+//
+//  Created by CHEN KAIDI on 27/11/2015.
+//  Copyright © 2015 CHEN KAIDI. All rights reserved.
+//
+
+#import "PTOrderStateView.h"
+
+@implementation PTOrderStateView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
